51d194ff9c6780ba1c532e155e14b173f7d3f9eb
that-coder-dude <107093439+that-coder-dude@users.noreply.github.com>
2025-05-02 02:58:41 +0530

    Update index.js

--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -7,7 +7,7 @@ const api = axios.create({
   },
 });
 
-// Attach JWT on every request if present
+
 api.interceptors.request.use(config => {
   const token = localStorage.getItem('token');
   if (token) config.headers.Authorization = `Bearer ${token}`;
